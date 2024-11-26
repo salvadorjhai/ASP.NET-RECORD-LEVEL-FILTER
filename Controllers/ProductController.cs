5191b76515ca41cd199360fc0f291a8a6bb0d61e
salvadorjhai <jhai_salvador@yahoo.com>
2024-11-26 14:27:22 +0800

    dynamic status buttons

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -23,19 +23,51 @@ namespace WebAppTemplate.Controllers
             var js = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/product.json"), new UTF8Encoding(false));
             var model = JsonConvert.DeserializeObject<List<Product>>(js);
 
+            // Group by StatusLvl and get counts
+            var transBadge = model
+                .GroupBy(item => item.statuslvl)
+                .Select(group => new
+                {
+                    id = group.Key,
+                    count = group.Count()
+                }).ToList();
+
             if (param.ContainsKey("statuslvl"))
             {
                 model = new List<Product>(model.Where(x => x.statuslvl == int.Parse(param["statuslvl"].ToString())).ToList());
             }
 
-            return Content(JsonConvert.SerializeObject(new { data = model }), "application/json");
+            return Content(JsonConvert.SerializeObject(new { data = model, badgecount = transBadge }), "application/json");
         }
 
         public ActionResult GetById(int id)
         {
             var js = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/product.json"), new UTF8Encoding(false));
-            var model = JsonConvert.DeserializeObject<List<Product>>(js).Where(x => x.id == id).FirstOrDefault();
+            var list = JsonConvert.DeserializeObject<List<Product>>(js);
+            var model = list.FirstOrDefault(x => x.id == id);
             return Content(JsonConvert.SerializeObject(new { data = model }), "application/json");
+        }
+
+        public ActionResult UpdateStatusLevel(int id, int statusid)
+        {
+            var js = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/product.json"), new UTF8Encoding(false));
+            var list = JsonConvert.DeserializeObject<List<Product>>(js);
+            var model = list.FirstOrDefault(x => x.id == id);
+            if (model != null)
+            {
+                // update status
+                model.statuslvl = statusid;
+
+                // save
+                js = JsonConvert.SerializeObject(list);
+                System.IO.File.WriteAllText(Server.MapPath("~/App_Data/product.json"), js, new UTF8Encoding(false));
+
+                // respond
+                Response.StatusCode = 200;
+                return Content(JsonConvert.SerializeObject(new { data = model, result="success" }), "application/json");
+            }
+            Response.StatusCode = 404;
+            return Content(JsonConvert.SerializeObject(new { result = "Not Found" }), "application/json");
         }
 
     }
---
+++ Scripts/myscripts/custom.js
@@ -0,0 +1,114 @@
+﻿
+/**
+ * show sweetalert
+ * @param {any} title
+ * @param {any} text
+ * @param {any} icon success, error, warning, info, question
+ */
+function swal(title, text, icon) {
+    swal2({
+        title: title,
+        text: text,
+        icon: icon,
+        dangerMode: (icon == 'error' || icon == 'warning'),
+    });
+}
+
+/**
+ * show sweetalert
+ * @param {any} opt options
+ * @param {any} isConfirmedFunc function to call on btn confirmed click
+ * @param {any} isCancelledFunc function to call on btn cancel/dismissed click
+ */
+function swal2(opt, isConfirmedFunc = null, isCancelledFunc = null) {
+    if (opt.buttonsStyling == null) {
+        opt.buttonsStyling = true
+    }
+    if (opt.allowOutsideClick == null) {
+        opt.allowOutsideClick = false
+    }
+    if (opt.allowEscapeKey == null) {
+        opt.allowEscapeKey = false
+    }
+    if (opt.stopKeydownPropagation == null) {
+        opt.stopKeydownPropagation = true
+    }
+    if (opt.keydownListenerCapture == null) {
+        opt.keydownListenerCapture = true
+    }
+    if (opt.showCloseButton == null) {
+        opt.showCloseButton = true
+    }
+    if (opt.closeButtonHtml == null) {
+        opt.closeButtonHtml = ''
+    }
+
+    if (opt.customClass == null) {
+        // if dangerMode is set
+        if (opt.dangerMode != null) {
+            opt.customClass = {
+                confirmButton: `btn btn-danger swal-btn-sz text-uppercase`,
+                cancelButton: 'btn btn-success swal-btn-sz text-uppercase',
+            }
+        } else {
+            opt.customClass = {
+                confirmButton: `btn btn-success swal-btn-sz text-uppercase`,
+                cancelButton: 'btn btn-primary swal-btn-sz text-uppercase',
+            }
+        }
+    }
+
+    if (opt.confirmButtonText == null) {
+        opt.confirmButtonText = `<i class="far fa-thumbs-up"></i> Okay`
+        opt.focusConfirm = true;
+    }
+
+    if (!(isConfirmedFunc === null)) {
+        if (opt.showCancelButton == null) {
+            opt.showCancelButton = true;
+        }
+        opt.focusConfirm = false;
+        opt.focusCancel = true;
+    }
+
+    if (opt.showCancelButton && !opt.cancelButtonText) {
+        opt.cancelButtonText = '<i class="far fa-thumbs-down"></i> Cancel';
+    }
+
+    if (opt.text && !opt.html) {
+        opt.html = opt.text.replace('\n', '<br>');
+        delete opt.text
+    }
+
+    if (!opt.returnFocus) {
+        opt.returnFocus = false;
+    }
+
+    delete opt.dangerMode; //  no longer supported in sweetalert2
+
+    Swal.fire(opt).then((result) => {
+        if (result.isConfirmed) {
+            setTimeout(isConfirmedFunc, 1);
+        } else {
+            setTimeout(isCancelledFunc, 1);
+        }
+    });
+}
+
+function ShowSwalLoader(title = 'Please wait...', text = 'Your request is being processed.') {
+    Swal.fire({
+        icon: 'info',
+        title: title,
+        text: text,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: false,
+        willOpen: () => {
+            Swal.showLoading();
+        }
+    });
+}
+
+function CloseSwalLoader() {
+    Swal.close()
+}
