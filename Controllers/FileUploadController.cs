98edb94e6604b02ab4a660f0381d8d48ddb50ba3
salvadorjhai <jhai_salvador@yahoo.com>
2024-12-22 10:29:55 +0800

    attachment js

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTemplate.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            return Content("Uploaded", "application/json");
+        }
+
+        [HttpGet]
+        public ActionResult Download()
+        {
+            return Content("Uploaded", "application/json");
+        }
+
+    }
+}
